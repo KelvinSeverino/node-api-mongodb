8795a733126eb664ada51d60bec632590ae548b6
Kelvin Severino <kseveri@outlook.com>
2024-05-27 09:51:16 -0300

    Rotas e gitIgnore

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from 'http';
 
 const PORT = 3000;
 
+const routes = {
+    "/": "Curso de Express API",
+    "/livros": "Rota Livros",
+    "/autores": "Rota Autores"
+};
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Curso de Node.js");
+    res.end(routes[req.url]);
 });
 
 server.listen(PORT, () => {
