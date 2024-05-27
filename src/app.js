2053835272c1816aac7641166995f4895907243d
Kelvin Severino <kseveri@outlook.com>
2024-05-27 10:27:16 -0300

    Store Livros

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 const books = [
     {
@@ -19,6 +20,11 @@ app.get("/", (req, res) => {
 
 app.get("/livros", (req, res) => {
     res.status(200).json(books);
+});
+
+app.post("/livros", (req, res) => {
+    books.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso!");
 });
 
 export default app;
