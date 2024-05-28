1a21b2708b4b6ed2494f856b365cac82f38ef0b5
Kelvin Severino <kseveri@outlook.com>
2024-05-28 09:31:44 -0300

    Erros de validação

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -3,6 +3,12 @@ import mongoose from "mongoose";
 function errorHandler (error, req, res, next) {       
     if(error instanceof mongoose.Error.CastError) {
         res.status(400).send({message: "Um ou mais dados fornecidos estão incorretos"});      
+    } else if (error instanceof mongoose.Error.ValidationError) {
+        const errorsMessage = Object.values(error.errors)
+                                    .map(error => error.message)
+                                    .join("; ");
+
+        res.status(400).send({message: `Os seguintes erros foram encontrados: ${errorsMessage}`})
     } else {
         res.status(500).send({message: `${error.message} - Falha na requisicao`});  
     };
