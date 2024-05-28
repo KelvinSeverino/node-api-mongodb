e1c45d39bebcceed1c1975bc951ac5d1b262684b
Kelvin Severino <kseveri@outlook.com>
2024-05-28 12:23:22 -0300

    Validacoes do mongoose na model Book

--- src/models/Book.js
+++ src/models/Book.js
@@ -7,9 +7,19 @@ const bookSchema = new mongoose.Schema({
         type: String, 
         required: [true, "O nome do livro é obrigatório"],
     },
-    publisher: { type: String },
+    publisher: { 
+        type: String,
+        enum: {
+            values: ["Casa do código", "Alura"],
+            message: "A editora {VALUE} fornecida não é valida"
+        }
+    },
     price: { type: Number },
-    pages: { type: Number },
+    pages: { 
+        type: Number,
+        min: [10, 'O Numero deve paginas estar entre 10 e 5000. Valor fornecido: {VALUE}'],
+        max: [5000, 'O Numero deve paginas estar entre 10 e 5000. Valor fornecido: {VALUE}'],
+    },
     author: authorSchema
 }, { versionKey: false });
 
